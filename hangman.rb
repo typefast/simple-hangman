d5825f78f31272286c2e4ffd0b030e0c56895383
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-02 10:49:23 +0000

    display_word method

--- hangman.rb
+++ hangman.rb
@@ -1,11 +1,19 @@
 class Hangman
+  attr_reader :guessed_letters_list
+  def initialize
+    @guessed_letters_list = []
+  end
   
   def random_word(array_words)
     array_words.sample
   end
   
-  
+  def display_word(word, letters)
+    letters = @guessed_letters_list
+    subbed = word.gsub(/[^#{letters}]/, ' _ ')
+    puts subbed
+  end
 end
 
 hangman = Hangman.new
-puts word = hangman.random_word(['hello','love'])
+word = hangman.random_word(['hello','love'])
